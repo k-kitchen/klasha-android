7223da5747ad3501dac4d540a240056fd9d8cfd6
m0sesa <adewale.moses.b@gmail.com>
2022-05-22 23:30:19 +0100

    wallet view error handling

--- androidsdk/src/main/java/com/klasha/android/ui/WalletLoginActivity.kt
+++ androidsdk/src/main/java/com/klasha/android/ui/WalletLoginActivity.kt
@@ -20,13 +20,41 @@ class WalletLoginActivity : AppCompatActivity() {
         val extras = intent.extras
         if (extras != null){
             binding.symbol.text = extras.getString("symbol","")
-            binding.amount.text = extras.getDouble("amount",0.0).toString()
+            binding.amount.text = "%,.2f".format(extras.getDouble("amount",0.0))
             binding.email.text = extras.getString("email","")
         }
 
         binding.buttonLogin.setOnClickListener {
             val username = binding.username.text.toString()
             val password = binding.password.text.toString()
+
+            binding.usernameLayout.errorIconDrawable = null
+            binding.usernameLayout.isErrorEnabled = false
+            binding.username.error = null
+            binding.passwordLayout.errorIconDrawable = null
+            binding.passwordLayout.isErrorEnabled = false
+            binding.password.error = null
+
+            if (username.isEmpty()){
+                binding.usernameLayout.isErrorEnabled = true
+
+                binding.usernameLayout.error = "Email can not be empty"
+                return@setOnClickListener
+            }
+
+            if (!android.util.Patterns.EMAIL_ADDRESS.matcher(username).matches()){
+                binding.usernameLayout.isErrorEnabled = true
+
+                binding.usernameLayout.error = "Please enter a valid email address"
+                return@setOnClickListener
+            }
+
+            if (password.isEmpty()){
+                binding.passwordLayout.isErrorEnabled = true
+
+                binding.passwordLayout.error = "Password can not be empty"
+                return@setOnClickListener
+            }
 
             val login = Login(username, password)
             submit(login)
